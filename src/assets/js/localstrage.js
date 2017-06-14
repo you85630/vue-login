2c09616e0b88578d73f42aee871ca46078c5fa1f
You <you85630@foxmail.com>
2017-06-14 18:31:26 +0800

    存数据

--- src/assets/js/localstrage.js
+++ src/assets/js/localstrage.js
@@ -1,7 +1,7 @@
 const STORAGE_KEY = 'data'
 export default {
-  fetch () {
-    return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+  remove () {
+    localStorage.clear()
   },
   save (items) {
     window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
