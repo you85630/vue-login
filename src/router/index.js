2139df9d34664478dab0d6234032dca90b454199
You <you85630@foxmail.com>
2017-08-15 11:25:43 +0800

    修改打包文件

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ const Login = resolve => require(['components/pages/login'], resolve)
 const Index = resolve => require(['components/pages/index'], resolve)
 const List = resolve => require(['components/pages/list'], resolve)
 const Edit = resolve => require(['components/pages/edit'], resolve)
-const Chart = resolve => require(['components/pages/chart'], resolve)
 
 const router = new Router({
   routes: [
@@ -34,13 +33,6 @@ const router = new Router({
       path: '/list/edit/:editId',
       name: 'edit',
       component: Edit,
-      meta: {
-        requireAuth: true
-      }
-    }, {
-      path: '/chart',
-      name: 'chart',
-      component: Chart,
       meta: {
         requireAuth: true
       }
